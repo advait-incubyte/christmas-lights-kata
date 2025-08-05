2a6b9592ef40f5498a419ecfcf537322a1638b9f
Advait Maindalkar <amaindalkar@kyruus.com>
2025-08-06 01:03:25 +0530

    test: fixed test case labels

--- test/christmas-lights.spec.ts
+++ test/christmas-lights.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import ChristmasLights from '../src/christmas-lights';
 
 describe('Christmas Lights', () => {
-  it('should light up all lights for 0,0 to 999,999', () => {
+  it('should turn on all lights from 0,0 to 999,999', () => {
     const christmasLights = new ChristmasLights();
     christmasLights.turnOn('0,0', '999,999');
     expect(christmasLights.getLightAt(0, 0)).toBe(true);
@@ -10,7 +10,7 @@ describe('Christmas Lights', () => {
     expect(christmasLights.getLightAt(500, 500)).toBe(true);
   });
 
-  it('should not light up lights for 0,0 to 999,999 when mode is OFF', () => {
+  it('should toggle lights 0.0 to 999,0', () => {
     const christmasLights = new ChristmasLights();
     christmasLights.turnOn('0,0', '999,999');
     christmasLights.toggle('0,0', '999,0');
@@ -19,4 +19,6 @@ describe('Christmas Lights', () => {
     expect(christmasLights.getLightAt(999, 0)).toBe(false);
     expect(christmasLights.getLightAt(500, 500)).toBe(true);
   });
+
+  it('s');
 });
